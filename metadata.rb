24b859620537b3a9e1c86a534546ed1a1ceeb257
Seth Vargo <sethvargo@gmail.com>
2013-08-28 17:54:53 -0400

    Bump devodd release

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs rsync"
-version           "0.8.2"
+version           "0.8.3"
 
 
 recipe "rsync", "Installs rsync, Provides LWRP rsync_serve for serivng pats via rsyncd"
