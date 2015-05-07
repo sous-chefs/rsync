de8eddd9d2a05d9d108c3be833d20ac9efe21cb2
Thom May <thom@may.lt>
2015-05-07 12:44:02 +0100

    support chef 12
    Fixes #13, fixes #14, fixes #17

--- providers/serve.rb
+++ providers/serve.rb
@@ -24,39 +24,38 @@ action :remove do
   write_conf
 end
 
-protected
-  #
-  # Walk collection for :add rsync_serve resources
-  # Build and write the config template
-  #
-  def write_conf
-    t = template(new_resource.config_path) do
-      source   'rsyncd.conf.erb'
-      cookbook 'rsync'
-      owner    'root'
-      group    'root'
-      mode     '0640'
-      variables(
-        :globals => global_modules,
-        :modules => rsync_modules
-      )
-      notifies :restart, "service[#{node['rsyncd']['service']}]", :delayed
-    end
-
-    new_resource.updated_by_last_action(t.updated?)
-
-    service node['rsyncd']['service'] do
-      action :nothing
-    end
+#
+# Walk collection for :add rsync_serve resources
+# Build and write the config template
+#
+def write_conf
+  t = template(new_resource.config_path) do
+    source   'rsyncd.conf.erb'
+    cookbook 'rsync'
+    owner    'root'
+    group    'root'
+    mode     '0640'
+    variables(
+      :globals => global_modules,
+      :modules => rsync_modules
+    )
+    notifies :restart, "service[#{node['rsyncd']['service']}]", :delayed
   end
 
-  # The list of attributes for this resource.
-  #
-  # @todo find a better way to do this
-  #
-  # @return [Array<String>]
-  def resource_attributes
-    %w(
+  new_resource.updated_by_last_action(t.updated?)
+
+  service node['rsyncd']['service'] do
+    action :nothing
+  end
+end
+
+# The list of attributes for this resource.
+#
+# @todo find a better way to do this
+#
+# @return [Array<String>]
+def resource_attributes
+  %w(
       auth_users
       comment
       dont_compress
@@ -85,51 +84,51 @@ protected
       uid
       use_chroot
       write_only
-    )
-  end
+  )
+end
 
-  # The list of rsync server resources in the resource collection
-  #
-  # @return [Array<Chef::Resource>]
-  def rsync_resources
-    run_context.resource_collection.select do |resource|
-      resource.is_a?(Chef::Resource::RsyncServe)
-    end
+# The list of rsync server resources in the resource collection
+#
+# @return [Array<Chef::Resource>]
+def rsync_resources
+  run_context.resource_collection.select do |resource|
+    resource.is_a?(Chef::Resource::RsyncServe)
   end
+end
 
-  # Expand "snake_case_things" to "snake case things".
-  #
-  # @param [String] string
-  #
-  # @return [String]
-  def snake_to_space(string)
-    string.to_s.gsub(/_/, ' ')
-  end
+# Expand "snake_case_things" to "snake case things".
+#
+# @param [String] string
+#
+# @return [String]
+def snake_to_space(string)
+  string.to_s.gsub(/_/, ' ')
+end
 
-  # The list of rsync modules defined in the resource collection.
-  #
-  # @return [Hash]
-  def rsync_modules
-    rsync_resources.reduce({}) do |hash, resource|
-      if resource.config_path == new_resource.config_path && resource.action == :add
-        hash[resource.name] ||= {}
-        resource_attributes.each do |key|
-          value = resource.send(key)
-          next if value.nil?
-          hash[resource.name][snake_to_space(key)] = value
-        end
+# The list of rsync modules defined in the resource collection.
+#
+# @return [Hash]
+def rsync_modules
+  rsync_resources.reduce({}) do |hash, resource|
+    if resource.config_path == new_resource.config_path && resource.action == :add
+      hash[resource.name] ||= {}
+      resource_attributes.each do |key|
+        value = resource.send(key)
+        next if value.nil?
+        hash[resource.name][snake_to_space(key)] = value
       end
-
-      hash
     end
-  end
 
-  # The global rsync configuration
-  #
-  # @return [Hash]
-  def global_modules
-    node['rsyncd']['globals'].reduce({}) do |hash, (key, value)|
-      hash[snake_to_space(key)] = value unless value.nil?
-      hash
-    end
+    hash
   end
+end
+
+# The global rsync configuration
+#
+# @return [Hash]
+def global_modules
+  node['rsyncd']['globals'].reduce({}) do |hash, (key, value)|
+    hash[snake_to_space(key)] = value unless value.nil?
+    hash
+  end
+end
