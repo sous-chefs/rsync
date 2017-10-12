7db6f4ba230f74744fb45e75ca512ef8ad082341
Jeremy MAURO <j.mauro@criteo.com>
2017-10-12 19:37:38 +0200

    Add spec test for systemd services
    - Add "mock_service_resource_providers" into spec_helper
    - Add Rhel7 systemd spec tests
    
    Change-Id: I9b7fc97cba84623c5be716fa999e7f2a7972c4b2
    Signed-off-by: Jeremy MAURO <j.mauro@criteo.com>

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -6,34 +6,56 @@ describe 'rsync::server' do
     allow(File).to receive(:exist?).with('/etc/rsyncd.conf').and_return(true)
   end
 
-  context 'on rhel' do
-    cached(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'centos', version: '6.8').converge('rsync::server')
-    end
-
-    it 'includes the default recipe' do
-      expect(chef_run).to include_recipe('rsync::default')
-    end
-
-    context '/etc/init.d/rsyncd template' do
-      let(:template) { chef_run.template('/etc/init.d/rsyncd') }
-
-      it 'writes the template' do
-        expect(chef_run).to render_file('/etc/init.d/rsyncd').with_content('Rsyncd init script')
+  {
+    '6.8' => %i(upstart redhat),
+    '7.4.1708' => %i(redhat systemd),
+  }.each do |os_version, service_providers|
+    context "on rhel #{os_version}" do
+      before do
+        mock_service_resource_providers(service_providers)
       end
 
-      it 'is owned by root:root' do
-        expect(template.owner).to eq('root')
-        expect(template.group).to eq('root')
+      cached(:chef_run) do
+        ChefSpec::SoloRunner.new(
+          platform: 'centos',
+          platform_family: 'rhel',
+          version: os_version
+        ).converge('rsync::server')
       end
 
-      it 'has the correct permissions' do
-        expect(template.mode).to eq('0755')
+      it 'includes the default recipe' do
+        expect(chef_run).to include_recipe('rsync::default')
       end
-    end
 
-    it 'starts and enables the rsync service' do
-      expect(chef_run).to enable_service('rsyncd')
+      context '/etc/init.d/rsyncd template' do
+        let(:template) { chef_run.template('/etc/init.d/rsyncd') }
+
+        case os_version
+        when /^6/
+          it 'writes the template' do
+            expect(chef_run).to render_file('/etc/init.d/rsyncd').with_content('Rsyncd init script')
+          end
+
+          it 'is owned by root:root' do
+            expect(template.owner).to eq('root')
+            expect(template.group).to eq('root')
+          end
+
+          it 'has the correct permissions' do
+            expect(template.mode).to eq('0755')
+          end
+        else
+          it 'not to write template' do
+            expect(chef_run).not_to render_file('/etc/init.d/rsyncd')
+          end
+        end
+      end
+      context 'rsyncd services' do
+        service_name = os_version =~ /^6/ ? 'rsyncd' : 'rsyncd.socket'
+        it "starts and enables the #{service_name} service" do
+          expect(chef_run).to enable_service(service_name)
+        end
+      end
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,7 @@ RSpec.configure do |config|
   config.formatter = :documentation # Use the specified formatter
   config.log_level = :error         # Avoid deprecation notice SPAM
 end
+
+def mock_service_resource_providers(hints)
+  allow(Chef::Platform::ServiceHelpers).to receive(:service_resource_providers).and_return(hints)
+end
