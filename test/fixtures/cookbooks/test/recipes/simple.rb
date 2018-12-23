eb20bebad15e078a0e7cd6d92b87e7ee85e81f46
Cédric Félizard <cedric@felizard.fr>
2018-12-23 11:32:46 +0000

    Sort modules alphabetically to preserve idempotency
    Signed-off-by: Cédric Félizard <cedric@felizard.fr>

--- test/fixtures/cookbooks/test/recipes/simple.rb
+++ test/fixtures/cookbooks/test/recipes/simple.rb
@@ -2,6 +2,10 @@ apt_update 'update' if platform_family?('debian')
 
 include_recipe 'rsync::server'
 
+rsync_serve 'foo' do
+  path '/foo'
+end
+
 rsync_serve 'tmp' do
   path '/tmp'
 end
--- test/integration/simple/default_spec.rb
+++ test/integration/simple/default_spec.rb
@@ -6,8 +6,8 @@ describe command('ps aux | grep rsync') do
   its('exit_status') { should eq 0 }
 end
 
-# rsync is serving /tmp
+# rsync is serving /foo and /tmp alphabetically
 describe command('rsync rsync://127.0.0.1') do
-  its('stdout') { should match /tmp/ }
+  its('stdout') { should match /foo\s*\ntmp/ }
   its('exit_status') { should eq 0 }
 end
