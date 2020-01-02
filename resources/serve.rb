66887fc981dc5b906d30ff629cabdabde0d25404
Tim Smith <tsmith84@gmail.com>
2020-01-02 14:47:35 -0800

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/serve.rb
+++ resources/serve.rb
@@ -13,9 +13,9 @@ attribute :name, kind_of: String, name_attribute: true
 attribute :config_path, kind_of: String, default: '/etc/rsyncd.conf'
 attribute :path, kind_of: String, required: true
 attribute :comment, kind_of: String
-attribute :read_only, kind_of: [TrueClass, FalseClass]
-attribute :write_only, kind_of: [TrueClass, FalseClass]
-attribute :list, kind_of: [TrueClass, FalseClass]
+attribute :read_only, kind_of: [true, false]
+attribute :write_only, kind_of: [true, false]
+attribute :list, kind_of: [true, false]
 attribute :uid, kind_of: String
 attribute :gid, kind_of: String
 attribute :auth_users, kind_of: String
@@ -24,17 +24,17 @@ attribute :hosts_allow, kind_of: String
 attribute :hosts_deny, kind_of: String
 attribute :max_connections, kind_of: Integer, default: 0
 attribute :munge_symlinks, kind_of: [TrueClass, FalseClass], default: true
-attribute :use_chroot, kind_of: [TrueClass, FalseClass]
+attribute :use_chroot, kind_of: [true, false]
 attribute :numeric_ids, kind_of: [TrueClass, FalseClass], default: true
-attribute :fake_super, kind_of: [TrueClass, FalseClass]
+attribute :fake_super, kind_of: [true, false]
 attribute :exclude_from, kind_of: String
 attribute :exclude, kind_of: String
 attribute :include_from, kind_of: String
 attribute :include, kind_of: String
-attribute :strict_modes, kind_of: [TrueClass, FalseClass]
+attribute :strict_modes, kind_of: [true, false]
 attribute :log_file, kind_of: String
 attribute :log_format, kind_of: String
-attribute :transfer_logging, kind_of: [TrueClass, FalseClass]
+attribute :transfer_logging, kind_of: [true, false]
 # by default rsync sets no client timeout (lets client choose, but this is a trivial DOS) so we make a 10 minute one
 attribute :timeout, kind_of: Integer, default: 600
 attribute :dont_compress, kind_of: String
--- test/integration/networking/default_spec.rb
+++ test/integration/networking/default_spec.rb
@@ -13,19 +13,19 @@ describe command('rsync rsync://127.0.0.1') do
 end
 
 describe file('/etc/rsyncd.conf') do
-  its('content') { should match /path = \/data\/repos\/prod\/centos/ } # rsync added path
+  its('content') { should match %r{path = /data/repos/prod/centos} } # rsync added path
   its('content')  { should match /comment = CentOS prod mirror/ } # rsync added comment
   its('content')  { should match /read only = true/ } # rsync is readonly
   its('content')  { should match /list = true/ } # rsync has list true
   its('content')  { should match /uid = nobody/ } # rsync is using nobody:nobody
   its('content')  { should match /gid = nobody/ } # rsync is using nobody:nobody
-  its('content')  { should match /hosts allow = 127.0.0.1, 10.4.1.0\/24, 192.168.4.0\/24/ } # rsync networking is setup
-  its('content')  { should match /hosts deny = 0.0.0.0\/0/ } # rsync networking is setup
+  its('content')  { should match %r{hosts allow = 127.0.0.1, 10.4.1.0/24, 192.168.4.0/24} } # rsync networking is setup
+  its('content')  { should match %r{hosts deny = 0.0.0.0/0} } # rsync networking is setup
   its('content')  { should match /max connections = 10/ } # rsync networking is setup
   its('content')  { should match /transfer logging = true/ } # rsync networking is setup
-  its('content')  { should match /log file = \/tmp\/centos-sync/ } # rsync networking is setup
-  its('content')  { should match /pre-xfer exec = \/bin\/true/ } # rsync networking is setup
-  its('content')  { should match /post-xfer exec = \/bin\/true/ } # rsync networking is setup
+  its('content')  { should match %r{log file = /tmp/centos-sync} } # rsync networking is setup
+  its('content')  { should match %r{pre-xfer exec = /bin/true} } # rsync networking is setup
+  its('content')  { should match %r{post-xfer exec = /bin/true} } # rsync networking is setup
   its('content')  { should match /incoming chmod = a=r,u\+w,D\+x/ } # rsync networking is setup
   its('content')  { should match /outgoing chmod = a=r,u\+w,D\+x/ } # rsync networking is setup
 end
